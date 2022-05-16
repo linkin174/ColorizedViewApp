ccce8854170ed61400bef170578dc328bf0dd388
Aleksandr Kretov <kretov.1988@gmail.com>
2022-05-16 15:59:14 +0500

    Initial Setup, Rect added

---
+++ ColorizedViewApp/ColorizedRectangleView.swift
@@ -0,0 +1,32 @@
+//
+//  ColorizedRectangleView.swift
+//  ColorizedViewApp
+//
+//  Created by Aleksandr Kretov on 16.05.2022.
+//
+
+import SwiftUI
+
+struct ColorizedRectangleView: View {
+    
+    let bounds = UIScreen.main.bounds
+    let redValue: Double
+    let greenValue: Double
+    let blueValue: Double
+    
+    var body: some View {
+        let mainColor = Color(red: redValue / 255,
+                                    green: greenValue / 255,
+                                    blue: blueValue / 255)
+        RoundedRectangle(cornerRadius: 10, style: .continuous)
+            .frame(width: bounds.width * 0.9, height: bounds.height * 0.3)
+            .foregroundColor(mainColor)
+            .shadow(color: mainColor, radius: 10, x: 2, y: 5)
+    }
+}
+
+struct ColorizedRectangleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorizedRectangleView(redValue: 200, greenValue: 20, blueValue: 120)
+    }
+}
--- ColorizedViewApp/ContentView.swift
+++ ColorizedViewApp/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ColorizedRectangleView(redValue: 100, greenValue: 20, blueValue: 222)
     }
 }
 
