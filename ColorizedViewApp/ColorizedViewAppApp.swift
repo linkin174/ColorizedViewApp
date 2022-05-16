26cd525c2ea37803bc51cf67613446a287bcd00d
Aleksandr Kretov <kretov.1988@gmail.com>
2022-05-16 15:19:56 +0500

    Initial Commit

---
+++ ColorizedViewApp/ColorizedViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedViewAppApp.swift
+//  ColorizedViewApp
+//
+//  Created by Aleksandr Kretov on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
